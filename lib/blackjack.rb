850d88d732c29f1d85abb58220d3fb67798d7883
boruch d <dubywider@gmail.com>
2019-03-28 19:08:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,6 +50,12 @@ end
 #####################################################
 
 def runner
- e
+welcome
+  total = initial_round
+  until total > 21
+    total = hit?(total)
+  display_card_total(total)
+  end
+  end_game(total)
 end
     
